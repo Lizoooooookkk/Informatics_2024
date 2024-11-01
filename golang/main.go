4bd90c9b954371558a18068594c3aa322ffefba2
Lizoooooookkk <korolevnikita678@gmail.com>
2024-11-01 20:52:48 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,10 @@
 package main
 
+import "isuct.ru/informatics2024/Lab_4"
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Мошкова Елизавета Алексеевна")
+
+	Lab_4.RunLab4()
 }
