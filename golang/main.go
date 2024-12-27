6326dce6470a7da9c360605bd9f46fb969b77cab
korolevnikita678@gmail.com <korolevnikita678@gmail.com>
2024-12-28 02:26:03 +0300

    lab7

--- golang/main.go
+++ golang/main.go
@@ -5,10 +5,12 @@ import (
 
 	"isuct.ru/informatics2022/lab4"
 	"isuct.ru/informatics2022/lab6"
+	"isuct.ru/informatics2022/lab7"
 )
 
 func main() {
 	fmt.Println("Мошкова Елизавета Алексеевна ")
 	lab6.RunLab6()
 	lab4.RunLab4()
+	lab7.RunLab7()
 }
